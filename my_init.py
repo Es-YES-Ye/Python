d407df1b984cbc9d605c91775a0ffb41d7573cc0
Es-YES-Ye <96801244+Es-YES-Ye@users.noreply.github.com>
2022-03-22 14:30:15 +0900

    Add files via upload
    python-my_init-ML

---
+++ my_init.py
@@ -0,0 +1,19 @@
+from google.colab import drive
+drive.mount('/content/drive')
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.linear_model import LinearRegression, LogisticRegression
+from sklearn.preprocessing import PolynomialFeatures, StandardScaler
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import r2_score, mean_squared_error
+
+import warnings
+
+warnings.filterwarnings(action = "ignore")
+
+#%matplotlib inline
