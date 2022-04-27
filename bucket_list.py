c762c562a371ecda47821bb3017b8f4167919666
Es-YES-Ye <96801244+Es-YES-Ye@users.noreply.github.com>
2022-04-27 21:13:13 +0900

    aws-sample
    aws-sample

--- bucket_list.py
+++ bucket_list.py
@@ -6,4 +6,4 @@ response = s3.list_buckets()
 # Output the bucket names
 print('Existing buckets:')
 for bucket in response['Buckets']:
-    print(f'  {bucket["Name"]}')
+    print(f'  {bucket["Name"]}') 
